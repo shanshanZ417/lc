e84981557f81605c894d76ae81f9b8330d266cfd
Shanshan Zhang <shzhang@shutterstock.com>
2019-09-20 17:20:49 -0400

    add selection sort

--- sorting/selectionSort.java
+++ sorting/selectionSort.java
@@ -1,9 +1,38 @@
-
+package sorting;
 
 public class selectionSort {
-    
-    public selectionSort() {
-        // TODO Auto-generated constructor stub
+    public static void main(String[] args) {
+        selectionSort test = new selectionSort();
+        int[] a = {1,1,1,1,1,2,2,2};
+        int[] b = {1,7,9,4,6,2,10};
+        test.sort(a);
+        test.sort(b);
+        for (int as : a) {
+            System.out.println(as);
+        }
+        for (int bs : b) {
+            System.out.println(bs);
+        }
     }
-    
+    public void sort(int[] a) {
+        if (a == null || a.length == 0) {
+            return;
+        }
+        int min = 0;
+        for (int i = 0; i < a.length; i++) {
+            min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[min]) {
+                    min = j;
+                }
+            }
+            swap(a, i , min);
+        }
+   }
+    public void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
 }
