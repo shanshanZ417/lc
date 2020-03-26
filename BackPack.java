69631f8f02e5591018893405b092165290903c9f
Shanshan_misaki <sz438@cornell.edu>
2020-03-26 11:01:43 -0400

    Update BackPack.java

--- BackPack.java
+++ BackPack.java
@@ -1,27 +1,24 @@
-// "static void main" must be defined in a public class.
 public class Main {
     public static void main(String[] args) {
         int[] w = {2, 3, 5};
         System.out.println(backpack(w, 11));
     }
-    private static int backpack(int[] w, int pack) {
-        int n = w.length;
+    private static int backpack(int[] nums, int total) {
+        int n = nums.length;
         // 1 ~ n package to get weight j
-        boolean[][] backpack = new boolean[n + 1][pack + 1];
-        for (int i = 0; i <= n; i++) {
-            backpack[i][0] = true;
-        }
+        boolean[][] dp = new boolean[n + 1][total + 1];
+        dp[0][0] = true;
         for (int i = 1; i <= n; i++) {
-            for (int p = 1; p <= pack; p++) {
-                backpack[i][p] = backpack[i - 1][p];
-                if (w[i - 1] <= p && backpack[i - 1][p - w[i - 1]]) {
-                    backpack[i][p] = true;
+            for (int j = 1; j <= total; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (nums[i - 1] <= j && dp[i - 1][j - nums[i - 1]]) {
+                    dp[i][j] = true;
                 }
             }
         }
-        for (int p = pack; p >= 0; p--) {
-            if (backpack[n][p]) {
-                return p;
+        for (int i = total; i >= 0; i--) {
+            if (dp[n][i]) {
+                return i;
             }
         }
         return 0;
